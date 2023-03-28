d3898c76bd0df51a6b003ef5d9e469b494e970d4
yan <yan.ledoux@icloud.com>
2023-03-28 12:42:23 +0200

    productid

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/product.model';
+import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit{
+  myproduct!: Product;
+  id: number = 1;
+
+constructor( private productsService:ProductsService, private route: ActivatedRoute ) { 
+  this.route.params.subscribe(params => {
+    this.id = parseInt(params['id'])
+  });
+}
+
+ngOnInit() {
+  this.myproduct=this.productsService.getOneProduct(this.id);
+}
 
 }
